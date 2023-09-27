b0202a24c154a4a623a67db127ef8d7e6f2bcb28
Büşra Çakmakcı <busracakmakci95@gmail.com>
2023-09-27 11:25:13 +0300

    Add project files.

---
+++ Xproject/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyNamespace
+
+
+{
+    class MyClass
+    {
+
+        static void Main()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\XX.txt");
+                string line = sr.ReadLine();
+
+                while (line !=null)
+                {
+                    string[] lines = line.Split('\n');
+
+                    foreach (var lineItem in lines)
+                    {
+                        Match match = Regex.Match(lineItem, @"\(\w+\): ([\+0-9\-X ]+)");
+
+                        if (match.Success)
+                        {
+                            string phoneNumber = match.Groups[1].Value;
+                            phoneNumber = Regex.Replace(phoneNumber, "[^X ]", "");
+                            Console.WriteLine(phoneNumber);
+                        }
+                    }
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+    }
+}
